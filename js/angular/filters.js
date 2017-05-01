0128ce153b152cf82ab454860d419eb290174d4f
pranjalsingi <pranjalsingi710@gmail.com>
2017-05-01 19:25:43 -0400

    filtered the courses by courseID

--- js/angular/filters.js
+++ js/angular/filters.js
@@ -40,3 +40,21 @@ app.filter('hasTags', function() {
     return output;
   };
 });
+
+app.filter('toArray', function () {
+  return function (obj, addKey) {
+    if (!angular.isObject(obj)) return obj;
+    if ( addKey === false ) {
+      return Object.keys(obj).map(function(key) {
+        return obj[key];
+      });
+    } else {
+      return Object.keys(obj).map(function (key) {
+        var value = obj[key];
+        return angular.isObject(value) ?
+          Object.defineProperty(value, '$key', { enumerable: false, value: key}) :
+          { $key: key, $value: value };
+      });
+    }
+  };
+});
